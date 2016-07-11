d74dfa6a6b2c232d08f3233bca06ffa1ed01f372
Anikina Viktory <roman-kosov@MacBook-Air-R-2.local>
2016-07-12 00:54:59 +0900

    test

---
+++ First Test/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  First Test
+//
+//  Created by R K on 11.07.16.
+//  Copyright © 2016 VikPik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var sdscxzc: UIButton!
+    
+        {
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
